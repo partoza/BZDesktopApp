b5f7e745a28228e9cfafced5584418308fc3a2e5
izdewww <andrelaurenceg@gmail.com>
2025-11-06 17:52:05 +0800

    update

---
+++ BZDesktopApp.Shared/Dtos/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BZDesktopApp.Shared.Dtos
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+    }
+}
