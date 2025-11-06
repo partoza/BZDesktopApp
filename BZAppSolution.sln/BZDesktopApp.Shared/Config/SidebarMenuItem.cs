276e27ce10aa2ecda5c5b5fe61bb2e92c1f717a0
izdewww <andrelaurenceg@gmail.com>
2025-11-06 18:13:15 +0800

    all update try

--- BZAppSolution.sln/BZDesktopApp.Shared/Config/SidebarMenuItem.cs
+++
@@ -1,8 +0,0 @@
-﻿public class SidebarMenuItem
-{
-    public string Title { get; set; } = "";
-    public string? Uri { get; set; }
-    public string? IconSvg { get; set; }
-    public List<string>? Roles { get; set; }
-    public List<SidebarMenuItem>? Children { get; set; }
-}
